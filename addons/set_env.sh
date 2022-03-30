f05de3169be9d58f645f8f8e967e5e28c3a598e0
Dawr82 <gerfo776@gmail.com>
2022-03-30 23:49:22 +0200

    Enabled addons directory temporarily

---
+++ addons/set_env.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+# Sets environmental variables necessary for docker-compose to work properly
+
+export MONGO_CONFIG=/mnt/c/Users/komputer/Documents/Learning/Python/Projekty/job-offer-collector/addons/mongo_config/.env
+export API_CONFIG=/mnt/c/Users/komputer/Documents/Learning/Python/Projekty/job-offer-collector/addons/api_config/.env
