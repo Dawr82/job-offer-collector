b82313d59d70cfa6d2eee57264c57d229e022d27
Dawid <gerfo776@gmail.com>
2022-03-31 01:01:26 +0200

    Adjusted docker-compose.yml

--- test/set_env.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-# Sets environmental variables necessary for docker-compose to work properly
-
-export MONGO_CONFIG=~/project/test/mongo_config/.env
-export API_CONFIG=~/project/test/api_config/.env
