eadcc8a86b2ee84758d4dd418f3bcc17476a3f21
Dawid <gerfo776@gmail.com>
2022-03-31 00:30:00 +0200

    Fixed build-related files for frontend service

--- test/set_env.sh
+++ test/set_env.sh
@@ -2,5 +2,5 @@
 
 # Sets environmental variables necessary for docker-compose to work properly
 
-export MONGO_CONFIG=/mnt/c/Users/komputer/Documents/Learning/Python/Projekty/job-offer-collector/addons/mongo_config/.env
-export API_CONFIG=/mnt/c/Users/komputer/Documents/Learning/Python/Projekty/job-offer-collector/addons/api_config/.env
+export MONGO_CONFIG=~/project/test/mongo_config/.env
+export API_CONFIG=~/project/test/api_config/.env
